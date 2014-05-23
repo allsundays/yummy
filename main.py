22dc76b5a0b4dc92a3e715a3f10e3353d166c64b
Alex.F <gfreezy@gmail.com>
2014-05-23 16:43:49 +0800

    import bookmarks

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 import tornado.ioloop
 import tornado.web
-from tornado.httpclient import AsyncHTTPClient
+import re
+from tornado.httpclient import AsyncHTTPClient, HTTPError
 from tornado import gen
 from readability.readability import Document
+
+
+HREF_REGEXP = re.compile(r'href=["\'](.*?)[\'"]', re.IGNORECASE)
+
+
+def extract(text):
+    try:
+        doc = Document(text)
+        article = doc.summary()
+        title = doc.short_title()
+        return title, article
+    except:
+        return None, None
 
 
 class MainHandler(tornado.web.RequestHandler):
@@ -16,20 +30,38 @@ class ExtractHandler(tornado.web.RequestHandler):
         url = self.get_argument('url', '')
         http_client = AsyncHTTPClient()
         resp = yield http_client.fetch(url)
-        title, article = self.extract(resp.body)
+        title, article = extract(resp.body)
         self.write(title)
         self.write(article)
 
-    def extract(self, text):
-        doc = Document(text)
-        article = doc.summary()
-        title = doc.short_title()
-        return title, article
+
+class ImportBookmarksHandler(tornado.web.RequestHandler):
+    def get(self):
+        return self.render('templates/import_bookmarks.html')
+
+    @gen.coroutine
+    def post(self):
+        bookmark_file = self.request.files.get('bookmark')[0]
+        body = bookmark_file.body
+        matches = HREF_REGEXP.finditer(body)
+        for m in matches:
+            url = m.group(1)
+            self.write('processing %s<br>' % url)
+            self.flush()
+            try:
+                resp = yield AsyncHTTPClient().fetch(url, connect_timeout=5)
+            except HTTPError:
+                self.write('fetch %s error' % url)
+                continue
+            title, article = extract(resp.body)
+            self.write('<p>title: %s</p>' % title)
+            self.flush()
 
 
 application = tornado.web.Application([
     (r"/", MainHandler),
     (r"/extract", ExtractHandler),
+    (r'/import', ImportBookmarksHandler),
 ],
     debug=True,
     autoreload=True,
