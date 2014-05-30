90f835be940df3e7bf260c52171bbf090585c677
Alex.F <gfreezy@gmail.com>
2014-05-30 16:38:57 +0800

    提取 get_and_extrace 函数；增加bookmark 方法

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import re
 import tornado.ioloop
 import tornado.web
 import html2text
+import logging
 from tornado.httpclient import AsyncHTTPClient
 from tornado import gen
 from readability.readability import Document
 from elasticsearch import Elasticsearch
 from search import index, search
+from charlockholmes import detect
 
 
 HREF_REGEXP = re.compile(r'href=["\'](.*?)[\'"]', re.IGNORECASE)
@@ -24,6 +26,7 @@ def extract(html):
             'full_text': html_to_text(html)
         }
     except:
+        logging.exception('extract html')
         return {}
 
 
@@ -35,9 +38,23 @@ def html_to_text(html):
     return clean_html.handle(html)
 
 
+@gen.coroutine
+def get_and_extract(url, timeout=5):
+    http_client = AsyncHTTPClient()
+    try:
+        resp = yield http_client.fetch(url, connect_timeout=timeout)
+    except:
+        logging.exception('fetch: %s' % url)
+        raise gen.Return({})
+    info = detect(resp.body)
+    body = resp.body.decode(info['encoding'], errors='ignore')
+
+    raise gen.Return(extract(body))
+
+
 class BaseHandler(tornado.web.RequestHandler):
     def get_current_user(self):
-        return self.get_secure_cookie("user")
+        return tornado.escape.xhtml_escape(self.get_secure_cookie("user"))
 
 
 class LoginHandler(BaseHandler):
@@ -57,24 +74,19 @@ class MainHandler(BaseHandler):
         if not self.current_user:
             self.redirect("/login")
             return
-        name = tornado.escape.xhtml_escape(self.current_user)
-        self.write("Hello, " + name)
+        self.write("Hello, " + self.current_user)
 
 
 class ExtractHandler(BaseHandler):
     @gen.coroutine
     def get(self):
         url = self.get_argument('url', '')
-        http_client = AsyncHTTPClient()
-        resp = yield http_client.fetch(url)
-        body = resp.body.decode('utf8')
-
-        inform = extract(body)
+        inform = yield get_and_extract(url)
         if not inform:
             self.finish('error')
             raise gen.Return()
 
-        self.write(
+        self.finish(
             u'''
             <p>{title}</p>
             <p>{article}</p>
@@ -91,7 +103,7 @@ class ImportBookmarksHandler(BaseHandler):
 
     @gen.coroutine
     def post(self):
-        user = tornado.escape.xhtml_escape(self.current_user)
+        user = self.current_user
         bookmark_file = self.request.files.get('bookmark')[0]
         body = bookmark_file.body
         matches = HREF_REGEXP.finditer(body)
@@ -99,12 +111,7 @@ class ImportBookmarksHandler(BaseHandler):
             url = m.group(1)
             self.write('processing %s<br>' % url)
             self.flush()
-            try:
-                resp = yield AsyncHTTPClient().fetch(url, connect_timeout=5)
-            except:
-                self.write('fetch %s error<br>' % url)
-                continue
-            inform = extract(resp.body)
+            inform = yield get_and_extract(url)
             if not inform:
                 continue
             title = inform['title']
@@ -118,7 +125,7 @@ class SearchHandler(BaseHandler):
         if not self.current_user:
             self.redirect("/login")
             return
-        user = tornado.escape.xhtml_escape(self.current_user)
+        user = self.current_user
         query = self.get_argument('query', '')
         if query:
             offset = int(self.get_argument('offset', 0))
@@ -129,12 +136,26 @@ class SearchHandler(BaseHandler):
             self.render("templates/search.html", query="")
 
 
+class AddBookmarkHandler(BaseHandler):
+    @gen.coroutine
+    def post(self):
+        url = self.get_argument('url')
+        inform = yield get_and_extract(url)
+        if not inform:
+            self.finish('error')
+
+        title = inform['title']
+        article = inform['article']
+        index(url, title, article, self.current_user)
+
+
 application = tornado.web.Application([
     (r"/", MainHandler),
     (r"/extract", ExtractHandler),
     (r'/import', ImportBookmarksHandler),
     (r"/login", LoginHandler),
     (r"/search", SearchHandler),
+    (r"/add", AddBookmarkHandler),
 ],
     debug=True,
     autoreload=True,
