9d64874e439e531f091316870fb9a08c06dee011
Alex.F <gfreezy@gmail.com>
2014-06-13 16:07:01 +0800

    重构代码

---
+++ main.py
@@ -0,0 +1,28 @@
+import tornado.ioloop
+from tornado.web import RedirectHandler
+
+from ctrls.web import RegisterHandler, LoginHandler, LogoutHandler, ExtractHandler, ImportBookmarksHandler, SearchHandler, AddBookmarkHandler
+
+
+application = tornado.web.Application([
+    (r"/", RedirectHandler, {'url': '/search'}, 'index'),
+    (r"/extract", ExtractHandler, {}, 'extract'),
+    (r'/import', ImportBookmarksHandler, {}, 'import'),
+    (r"/register", RegisterHandler, {}, 'register'),
+    (r"/login", LoginHandler, {}, 'login'),
+    (r"/logout", LogoutHandler, {}, 'logout'),
+    (r"/search", SearchHandler, {}, 'search'),
+    (r"/add", AddBookmarkHandler, {}, 'add'),
+    (r"/static/(.*)", tornado.web.StaticFileHandler, {"path": "static"})
+],
+    debug=True,
+    cookie_secret="dev@yummy",
+    xsrf_cookies=True,
+    login_url='/login',
+    template_path='templates',
+)
+
+
+if __name__ == "__main__":
+    application.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
